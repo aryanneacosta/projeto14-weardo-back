02bb8e9b007976d2dfce94c66f49754a0f6dec07
weillercarvalho <engweiller@hotmail.com>
2022-09-15 02:24:57 -0300

    FEAT [index.js] Logic part finished, focus on  middlewares,routes and controllers.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import bcrypt from 'bcrypt';
 import joi from 'joi';
 import { ServerStyleSheet } from 'styled-components';
 import { MongoClient } from 'mongodb';
+import {v4 as uuidv4} from 'uuid';
+
 
 dotenv.config();
 
@@ -21,11 +23,24 @@ mongoClient.connect().then(() => {
     db = mongoClient.db(`weardo`)
 })
 
+const singUpSchema = joi.object({
+    name: joi.string().empty(" ").min(3).max(30).required(),
+    email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'br'] } }).empty(" ").min(6).max(40).required(),
+    password: joi.string().empty(" ").min(6).max(30).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    address: joi.string().empty(" ").min(3).max(50).required(),
+});
+
 server.post(`/singup`, async (req,res) => {
-    const {name, age} = req.body;
+    const {name, email, password, address} = req.body;
+    const validation = singUpSchema.validate(req.body, {abortEarly:false});
+    if (validation.error) {
+        const erroMessage = validation.error.details.map(value => value.message);
+        return res.status(422).send(console.log(erroMessage))
+    }
     try {
-        await db.collection(`users`).insertOne({name: name});
-        return res.sendStatus(201);
+        const hashingPassword = bcrypt.hashSync(password,12);
+        await db.collection(`users`).insertOne({...req.body,password:hashingPassword});
+        return res.send(201)
     } catch (error) {
         return res.status(500).send(error.message)
     }
@@ -33,7 +48,21 @@ server.post(`/singup`, async (req,res) => {
 })
 
 server.post(`/singin`, async (req,res) => {
-    res.sendStatus(201);
+    const {email, password} = req.body;
+    try {
+        const user = await db.collection(`users`).findOne({email:email});
+        if (user && bcrypt.compareSync(password,user.password)) {
+            const token = uuidv4();
+            await db.collection(`sessions`).insertOne({userId: user._id, token, address: user.address});
+            return res.send({token: token, name: user.name});
+        }
+        else {
+            return res.sendStatus(401);
+        }
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+
 })
 
 
