a8dda84f434ec0e2ca0709b898280476bdd061d5
Aryanne Acosta <aryanneacosta@gmail.com>
2022-09-17 16:41:58 -0300

    rota get products

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import singrouter from './src/routers/singinupRoute.js';
+import productsrouter from './src/routers/productsRoute.js';
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(singrouter);
+server.use(productsrouter);
 
 server.listen(process.env.PORT, () => {
     console.log(`Listening on the port ${process.env.PORT}`)
